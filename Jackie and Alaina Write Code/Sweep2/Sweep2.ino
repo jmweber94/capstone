4fe7c76647549fe50c2b0da93a50c9ff30c05d2e
Jackie Weber <jmweber94@gmail.com>
2016-04-12 15:05:16 -0400

    changes

---
+++ Jackie and Alaina Write Code/Sweep2/Sweep2.ino
@@ -0,0 +1,44 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com> 
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://arduino.cc/en/Tutorial/Sweep
+*/ 
+
+#include <Servo.h> 
+
+ 
+Servo myservo;  // create servo object to control a servo 
+                // twelve servo objects can be created on most boards
+ 
+int pos = 0;    // variable to store the servo position 
+ 
+void setup() 
+{ 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  Serial.begin(9600);
+} 
+ 
+void loop() 
+{ 
+  if (Serial.available()) {  // Returns true if there is serial input.
+    char ch = Serial.read();
+    
+    if (ch == 'e') {
+      // Make sure not to exceed the mechanical limitation.
+      pos = 50;
+    } 
+    else if (ch == 'r'){
+      // Make sure not to exceed the mechanical limitation.
+      pos = 30;
+    }
+    
+    // Now ask the servo to move to that position.
+    myservo.write(pos);
+    // Mechnical limitation to the frequency of commands given.
+    delay(50);
+  }
+} 
+
---
+++ Jackie and Alaina Write Code/pseudo_code/pseudo_code.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+if (nfc code){
+  servo engage
+  motor retract
+  motor stop
+  alert nurse
+}
+
+
+
+
+
+}
